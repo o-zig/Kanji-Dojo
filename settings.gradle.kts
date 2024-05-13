6afe0e932b7c10840dd9efd6717ded7be00e4e79
SYtor <shuliak.yaroslav@gmail.com>
2024-05-13 12:06:36 +0300

    Update compose to v1.6.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,17 +7,17 @@ pluginManagement {
 
     val kotlinVersion = "1.9.0"
     val agpVersion = "8.1.2"
-    val composeVersion = "1.5.10"
+    val composeVersion = "1.6.2"
 
     plugins {
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("android") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-        id("org.jetbrains.compose").version(composeVersion)
-        id("com.google.gms.google-services").version("4.4.0")
-        id("com.google.firebase.crashlytics").version("2.9.9")
+        id("com.android.application") version agpVersion
+        id("com.android.library") version agpVersion
+        id("org.jetbrains.compose") version composeVersion
+        id("com.google.gms.google-services") version "4.4.0"
+        id("com.google.firebase.crashlytics") version "2.9.9"
         id("com.codingfeline.buildkonfig") version "0.13.3"
         id("app.cash.sqldelight") version "2.0.0"
     }
