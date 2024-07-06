e39e96cbc6371fb87be8b90ac629296d3d4029c2
syt0r <shuliak.yaroslav@gmail.com>
2024-07-06 12:09:25 +0300

    Make daily limit indicator non transparent

--- core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/screen/letters_dashboard/ui/LettersDashboardDailyLimitIndicator.kt
+++ core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/screen/letters_dashboard/ui/LettersDashboardDailyLimitIndicator.kt
@@ -1,6 +1,7 @@
 package ua.syt0r.kanji.presentation.screen.main.screen.home.screen.letters_dashboard.ui
 
 import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -10,11 +11,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import ua.syt0r.kanji.core.srs.DailyGoalConfiguration
+import ua.syt0r.kanji.presentation.common.resources.string.LettersDashboardStrings
 import ua.syt0r.kanji.presentation.common.resources.string.resolveString
 import ua.syt0r.kanji.presentation.common.theme.extraColorScheme
 import ua.syt0r.kanji.presentation.common.ui.CustomRippleTheme
@@ -26,87 +29,98 @@ fun LettersDashboardDailyLimitIndicator(
     data: DailyIndicatorData,
     updateConfiguration: (DailyGoalConfiguration) -> Unit
 ) {
+
+    var shouldShowDialog by remember { mutableStateOf(false) }
+    if (shouldShowDialog) {
+        DailyGoalDialog(
+            configuration = data.configuration,
+            onDismissRequest = { shouldShowDialog = false },
+            onUpdateConfiguration = {
+                updateConfiguration(it)
+                shouldShowDialog = false
+            }
+        )
+    }
+
     CompositionLocalProvider(
         LocalRippleTheme provides CustomRippleTheme(
             colorProvider = { MaterialTheme.colorScheme.onSurface }
         )
     ) {
-
-        val strings = resolveString { lettersDashboard }
-        val message = when {
-            data.progress is DailyProgress.Disabled -> buildAnnotatedString {
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(strings.dailyIndicatorPrefix)
-                }
-                withStyle(SpanStyle(MaterialTheme.colorScheme.outline)) {
-                    append(strings.dailyIndicatorDisabled)
-                }
-            }
-
-            data.progress is DailyProgress.Completed -> buildAnnotatedString {
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(strings.dailyIndicatorPrefix)
-                }
-                withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
-                    append(strings.dailyIndicatorCompleted)
-                }
-            }
-
-            data.progress is DailyProgress.StudyAndReview -> buildAnnotatedString {
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(strings.dailyIndicatorPrefix)
-                }
-                withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
-                    append(strings.dailyIndicatorNew(data.progress.study))
-                }
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(" • ")
-                }
-                withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
-                    append(strings.dailyIndicatorReview(data.progress.review))
-                }
-            }
-
-            data.progress is DailyProgress.StudyOnly -> buildAnnotatedString {
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(strings.dailyIndicatorPrefix)
-                }
-                withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
-                    append(strings.dailyIndicatorNew(data.progress.count))
-                }
-            }
-
-            data.progress is DailyProgress.ReviewOnly -> buildAnnotatedString {
-                withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
-                    append(strings.dailyIndicatorPrefix)
-                }
-                withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
-                    append(strings.dailyIndicatorReview(data.progress.count))
-                }
-            }
-
-            else -> throw IllegalStateException()
-        }
-
-        var shouldShowDialog by remember { mutableStateOf(false) }
-        if (shouldShowDialog) {
-            DailyGoalDialog(
-                configuration = data.configuration,
-                onDismissRequest = { shouldShowDialog = false },
-                onUpdateConfiguration = {
-                    updateConfiguration(it)
-                    shouldShowDialog = false
-                }
-            )
-        }
-
         TextButton(
-            onClick = { shouldShowDialog = true }
+            onClick = { shouldShowDialog = true },
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = MaterialTheme.colorScheme.surface.copy(0.9f)
+            )
         ) {
             Text(
-                text = message,
+                text = getIndicatorMessage(
+                    strings = resolveString { lettersDashboard },
+                    data = data
+                ),
                 fontWeight = FontWeight.Light
             )
+        }
+
+    }
+
+}
+
+@Composable
+private fun getIndicatorMessage(
+    strings: LettersDashboardStrings,
+    data: DailyIndicatorData
+): AnnotatedString {
+    return when (data.progress) {
+        is DailyProgress.Disabled -> buildAnnotatedString {
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(strings.dailyIndicatorPrefix)
+            }
+            withStyle(SpanStyle(MaterialTheme.colorScheme.outline)) {
+                append(strings.dailyIndicatorDisabled)
+            }
+        }
+
+        is DailyProgress.Completed -> buildAnnotatedString {
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(strings.dailyIndicatorPrefix)
+            }
+            withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
+                append(strings.dailyIndicatorCompleted)
+            }
+        }
+
+        is DailyProgress.StudyAndReview -> buildAnnotatedString {
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(strings.dailyIndicatorPrefix)
+            }
+            withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
+                append(strings.dailyIndicatorNew(data.progress.study))
+            }
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(" • ")
+            }
+            withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                append(strings.dailyIndicatorReview(data.progress.review))
+            }
+        }
+
+        is DailyProgress.StudyOnly -> buildAnnotatedString {
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(strings.dailyIndicatorPrefix)
+            }
+            withStyle(SpanStyle(MaterialTheme.extraColorScheme.success)) {
+                append(strings.dailyIndicatorNew(data.progress.count))
+            }
+        }
+
+        is DailyProgress.ReviewOnly -> buildAnnotatedString {
+            withStyle(SpanStyle(MaterialTheme.colorScheme.onSurface)) {
+                append(strings.dailyIndicatorPrefix)
+            }
+            withStyle(SpanStyle(MaterialTheme.colorScheme.primary)) {
+                append(strings.dailyIndicatorReview(data.progress.count))
+            }
         }
 
     }
