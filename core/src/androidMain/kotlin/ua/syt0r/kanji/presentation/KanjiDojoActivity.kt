c24d2302256cb5d1b0f106aa6db05134c7fd9435
syt0r <shuliak.yaroslav@gmail.com>
2024-07-15 17:25:47 +0300

    Basic edge-to-edge support

--- core/src/androidMain/kotlin/ua/syt0r/kanji/presentation/KanjiDojoActivity.kt
+++ core/src/androidMain/kotlin/ua/syt0r/kanji/presentation/KanjiDojoActivity.kt
@@ -2,12 +2,14 @@ package ua.syt0r.kanji.presentation
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 open class KanjiDojoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { KanjiDojoApp() }
     }
 
--- core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/KanjiDojoApp.kt
+++ core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/KanjiDojoApp.kt
@@ -1,8 +1,11 @@
 package ua.syt0r.kanji.presentation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import ua.syt0r.kanji.core.logger.Logger.getKoin
 import ua.syt0r.kanji.core.theme_manager.LocalThemeManager
 import ua.syt0r.kanji.core.theme_manager.ThemeManager
@@ -16,7 +19,11 @@ fun KanjiDojoApp(
     CompositionLocalProvider(LocalThemeManager provides themeManager) {
         AppTheme {
             Surface {
-                MainScreen()
+                Box(
+                    modifier = Modifier.safeDrawingPadding()
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
